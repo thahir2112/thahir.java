5d0b12b56ab09b890d365d40c0281a385b8a9fed
Mohamed Thahir <thahirmohamed212@gmail.com>
2025-09-18 11:06:06 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class addition{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num_1= sc.nextInt();
+        int num_2= sc.nextInt();
+        int num_3= sc.nextInt();
+        int a = num_1+num_2+num_3;
+        System.out.println(a);
+    }
+}
