2d2e99fe9496babe3af871bbe9fe70d3125c0d07
Mohamed Thahir <thahirmohamed212@gmail.com>
2025-09-18 09:27:17 +0530

    Create pattaambuchi.java

---
+++ pattaambuchi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class pattaambuchi {
+    public static void main(String [] args){
+    Scanner in = new Scanner (System.in);
+
+    int n = in.nextInt();
+
+    for ( int row = 1 ; row<=n; row++){
+        for ( int col = 1 ; col<row;col++){
+            System.out.print("*");
+        }
+        for( int col = 1 ; col<=2*(n-row);col++){
+            System.out.print(" ");
+        }
+        for ( int col = 1 ; col<row;col++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for ( int row = n-1 ; row>=1; row--){
+        for ( int col = 1 ; col<row;col++){
+            System.out.print("*");
+        }
+        for( int col = 1 ; col<=2*(n-row);col++){
+            System.out.print(" ");
+        }
+        for ( int col = 1 ; col<row;col++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    in.close();
+    }
+}
