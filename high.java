711624eea021b786cab8408111cb8e3682cc0f2d
Mohamed Thahir <thahirmohamed212@gmail.com>
2026-02-24 18:12:25 +0530

    Create high.java

---
+++ high.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class high {
+    public static void main(String args[]){
+     Scanner os = new Scanner(System.in);
+     int size = os.nextInt();
+     int arr[] = new int[size];
+     for(int i = 0 ; i <size ; i++)
+     arr[i] = os.nextInt();
+     for(int i = 0 ; i <size; i++){
+        if(arr[i] }
+     }
+    }
+}
