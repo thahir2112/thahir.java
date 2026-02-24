da79b623f2c6103f000f6045fbf4780a5dcfebaf
Mohamed Thahir <thahirmohamed212@gmail.com>
2026-02-24 18:20:49 +0530

    Create transition.java

---
+++ transition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class transition {
+    public static void main(String args[]){
+        Scanner odd = new Scanner(System.in);
+         int size = odd.nextInt();
+         int arr[] = new int[size];
+         for(int i = 0; i < size; i++){
+
+         }
+         }
+    } 
